2b77bf6be852f38587d17ca39f6a749ec99e8d10
anupampandey01 <pandeyanupam180@gmail.com>
2025-08-18 15:36:41 +0530

    hero-section

---
+++ client/src/component/KeyBenefit/KeyBenefit.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./KeyBenefit.css"
+
+const KeyBenefit = () => {
+  return (
+    <div className='key-benefit-section'>
+      KeyBenefit
+    </div>
+  )
+}
+
+export default KeyBenefit
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Home.css"
 import Hero from '../../component/Hero/Hero'
+import KeyBenefit from '../../component/KeyBenefit/KeyBenefit'
 const Home = () => {
   return (
     <>
