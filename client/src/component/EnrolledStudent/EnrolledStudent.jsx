bdf1c16f8e97afaa31a9f78076993797798b1fbf
anupampandey01 <pandeyanupam180@gmail.com>
2026-01-02 03:20:25 +0530

    fix: enrolled student

--- client/src/component/EnrolledStudent/EnrolledStudent.jsx
+++ client/src/component/EnrolledStudent/EnrolledStudent.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import "./EnrolledStudent.css"
 const EnrolledStudent = () => {
   return (
-    <div>
-      
+    <div className='ernrolled-student'>
+      We are working on it
     </div>
   )
 }
